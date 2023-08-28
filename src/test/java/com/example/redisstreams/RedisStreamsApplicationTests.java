09d1c71f569a9c61ae3735d50e52893718fdc421
HyuneyBee <gsh2448@gmail.com>
2023-08-28 21:20:38 +0900

    redis streams 를 이용한 Event-Driven 아키텍처 예제

---
+++ src/test/java/com/example/redisstreams/RedisStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
